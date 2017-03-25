4b18de7fbede2bf1b648d47876c96781d9bf7555
Kokila Alupotha <kokila@vesess.com>
2017-03-25 17:26:01 +0530

    Change path to isAdmin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,11 +30,11 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            //\Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\HTTP\Middleware\isAdmin::class,
+            //\App\HTTP\Middleware\isAdmin::class,
         ],
 
         'api' => [
@@ -52,7 +52,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'isAdmin' => \App\HTTP\Middleware\isAdmin::class,
+        'isAdmin' => Middleware\isAdmin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
